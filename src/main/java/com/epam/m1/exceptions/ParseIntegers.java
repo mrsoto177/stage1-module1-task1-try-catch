29689200a6235a41fa751e3cfce20c8b8dd9ff1c
Marcos Soto <you@example.com>
2024-04-24 12:49:33 -0600

    ST1 T1 1

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -5,27 +5,40 @@ import java.util.Iterator;
 import java.util.List;
 
 /**
- * Complete the code, parse integers, calculate the sum of numbers in the WORDS, join strings with
- * space delimiter
+ * Complete the code, parse integers, calculate the sum of numbers in the WORDS,
+ * join strings with space delimiter
  */
+
 public class ParseIntegers {
 
-    private static final List<String> WORDS =
-            Arrays.asList(
-                    "JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations."
-                            .split(" "));
+    private static final List<String> WORDS = Arrays.asList(
+            " JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations."
+                    .split(" "));
 
     public static void main(String[] args) {
-        Iterator<String> words = WORDS.iterator();
-        int sum = 0;
-        String justWords = "";
-        while (words.hasNext()) {
-            String next = words.next();
-            int number = Integer.parseInt(next);
-            // todo: complete it
+        try {
+            Iterator<String> words = WORDS.iterator();
+            int sum = 0;
+            String justWords = "";
+            while (words.hasNext()) {
+                String next = words.next();
+                try {
+                    int number = Integer.parseInt(next);
+                    sum += number;
+                    
+                } catch (NumberFormatException e) {
+                    // Si no se puede convertir a número, simplemente se agrega a justWords sin intentar la conversión
+                    justWords += next + " ";
+                }
+            }
+            System.out.println("Sum is " + sum);
+            System.out.println("Just words:" + justWords);
+
+        } catch (NumberFormatException e) {
+            System.out.println("Error:" + e.getMessage());
         }
-        System.out.println("Sum is " + sum);
-        System.out.println("Just words:" + justWords);
+
     }
 }
+
 
